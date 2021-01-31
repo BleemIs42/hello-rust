d985fc1c603181ff28dc32dde8da6ab0d6b1ab66
BarryYan <barry.yansh@gmail.com>
2021-01-31 13:21:53 +0800

    Add collections

---
+++ 08_collections/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    println!("Hello, Vector: {:?}", v);
+
+    let mut v1 = vec![1,2,3];
+    // v1.push(4);
+    println!("Hello, Vector: {:?}", v1);
+
+    for i in &mut v1 {
+        *i += 10;
+        println!("{}", i);
+    }
+
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+    println!("{}", hello);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("{}", s);
+
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+}
