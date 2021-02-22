117d2ef71f035ad7a4cbd665abdb0a437395b3f2
Shuanghui Yan <shuanghui.yan@thoughtworks.com>
2021-02-22 10:55:43 +0800

    Add cli code

--- 12_minigrep/src/main.rs
+++ 12_minigrep/src/main.rs
@@ -1,31 +1,20 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        panic!("not enough arguments");
-    }
-    
-    let config = Config::new(&args);
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file: {}");
-
-    println!("With text:\n\n{}", contents);
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config { query, filename }
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
