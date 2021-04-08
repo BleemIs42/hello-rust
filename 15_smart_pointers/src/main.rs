c0b4acd87b621df5b6995dd15f4699e039b0f426
BarryYan <barry.yansh@gmail.com>
2021-04-08 23:13:56 +0800

    Add smart pointers

---
+++ 15_smart_pointers/src/main.rs
@@ -0,0 +1,68 @@
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+// #[derive(Debug)]
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    box_example();
+    drop_example();
+    rc_example();
+}
+
+fn box_example(){
+    // let list = Cons(1,
+    //     Box::new(
+    //         Cons(2, Box::new(Nil))
+    //     )
+    // );
+    // println!("list val is {:#?}", list);
+}
+
+fn drop_example(){
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+
+    let cc = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    drop(cc);
+    println!("CustomSmartPointer dropped before the end of main.");
+}
+
+fn rc_example(){
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+}
